3fd9a968afed7977ffb4192dc12d1d24eb820af8
Christopher <161328235+IChris2611I@users.noreply.github.com>
2024-08-21 18:09:39 +0200

    Update Section.jsx

--- src/components/homePage/ui/Section.jsx
+++ src/components/homePage/ui/Section.jsx
@@ -1,7 +1,14 @@
 export default function SectionLeft() {
   return (
-    <div className="bg-white h-full w-full col-span-3 row-span-full">
-    
+    <div className="h-full w-full col-span-3 row-span-full flex flex-col">
+      <div>
+        <img
+          src="/logo.png"
+          alt="logo riot game"
+          className="w-22 h-10 justify-center items-center"
+        />
+      </div>
+      <div className="bg-red-900"></div>
     </div>
   )
 }
